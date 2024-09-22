fc8d73e8532da5b9d95c12742623b13ec0347f74
Anubhav Gain <anubhavg@infopercept.com>
2024-09-22 15:52:01 +0530

    update: loader transition for loader animation

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,11 @@
 import type { Site, Page } from './types'
 
+export const loaderAnimation = {
+  duration: 0.5,
+  opacity: [0, 1],
+  scale: [0.9, 1],
+  easing: "ease-in-out",
+};
 export const LINKS = {
   github: 'https://github.com/mranv',
   linkedin: 'https://www.linkedin.com/in/anubhavgain/',
